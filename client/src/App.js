906faf10c708a7c242eac529912e28a5466f3b66
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-06-13 20:52:08 +0530

    Added React Components

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import "./App.css";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <div className="container">
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/AddTransaction.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function AddTransaction() {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+  return (
+    <React.Fragment>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            placeholder="Enter amount..."
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default AddTransaction;
