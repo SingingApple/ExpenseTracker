3e46eee3fc3eafb8b690366ec943ddbbecafc2a7
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-06-15 00:50:15 +0530

    Completed Controllers

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -33,19 +33,50 @@ const addTransactions = async (req, res, next) => {
       success: true,
       data: transaction,
     });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: "Server Error",
-    });
+  } catch (err) {
+    // return res.status(500).json({
+    //   success: false,
+    //   error: "Server Error",
+    // });
+
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      return res.status(400).json({
+        success: false,
+        errors: messages,
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: err,
+      });
+    }
   }
 };
 
 // @desc DELETE transaction
 // @route DELETE /api/v1/transactions/
 //@access PUBLiC
-const deleteTransactions = (req, res, next) => {
-  res.send("ADD TRANSACTIONS");
+const deleteTransactions = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const transaction = await Transaction.findByIdAndRemove(id);
+    if (!transaction) {
+      return res.status(400).json({
+        success: false,
+        error: "No Transaction found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    res.status(201).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 module.exports = {
